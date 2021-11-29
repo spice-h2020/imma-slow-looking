5d2ce80b3e679301cb890d250debf28b2984d743
PaulMulholland <p.mulholland@open.ac.uk>
2021-11-29 16:59:48 +0000

    Debugging removed

--- src/app/currentUser.service.ts
+++ src/app/currentUser.service.ts
@@ -15,25 +15,19 @@ export class CurrentUser{
     public getUser(): User {
         let userstring = localStorage.getItem("user");
         let user = JSON.parse(userstring);
-        console.log("get user");
-        console.log(user);
-        console.log(this.user);
         return user;
     }
 
     public setUser(user: User) {
         let userstring = JSON.stringify(user);
         localStorage.setItem("user", userstring);
-        console.log("set user");
-        console.log(userstring);
-        this.user = user;
+
+        // this.user = user;
     }
 
     public logout() {
-        // localStorage.clear();
         this.user = new User;
         localStorage.setItem("user", JSON.stringify(this.user));
-        console.log("clear");
     }
 
 }
